4b4d89647de0937cf29b88b8b7b37daebc132937
BT_4N2_15 <Jelani7087@bths.edu>
2023-02-17 12:26:48 -0500

    Package and 1/3 of PostageCalculator

--- src/PostageCalculator.java
+++ src/PostageCalculator.java
@@ -1,17 +1,21 @@
 public class PostageCalculator extends Package {
-    public final static double BASE_SHIPPING_COST = 3.75;
 
     public static double calculatePostage(Package p) {
-        double shippingCost = BASE_SHIPPING_COST;
-
-
-
-        return shippingCost;
+        return 3.75 + costIfExceedingSize(p.length(), p.width(), p.height())
+                + costBasedOnWeight(p.weight()) + costBasedOnZipcode(p.origin().zipcode(), p.destination().zipcode());
     }
 
-    private double costBasedOnWeight(double weight) {
-        double cost = 0;
+    private static double costBasedOnWeight(double weight) {
+        if (weight > 40) return  (400 * 0.05) + ( ((weight - 40) /.1) * 0.1);
+        return (weight / .1) * 0.05;
+    }
 
-        return cost;
+    private static double costBasedOnZipcode(String zip1, String zip2) {
+        return Math.abs((Integer.parseInt(zip1.substring(0, 3)) - Integer.parseInt(zip2.substring(0, 3)))) / 100;
+    }
+
+    private static double costIfExceedingSize(double length, double width, double height) {
+        if (length + width + height > 36) return ((length + width + height) - 36) * .1;
+        return 0;
     }
 }
