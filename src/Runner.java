b17da6bc1d791acd5d9449a14b869578d0b05b41
BT_4N2_15 <Jelani7087@bths.edu>
2023-02-17 11:29:10 -0500

    j

--- src/Runner.java
+++ src/Runner.java
@@ -8,23 +8,29 @@ public class Runner {
         String streetNumber, streetName, apartmentNumber, city, state, zipcode;
 
 
-           System.out.print("Enter a street number: "); streetNumber = scanner.nextLine();
-           System.out.print("Enter a street name: "); streetName = scanner.nextLine();
+           System.out.print("Enter a street number: "); streetNumber = validateInput(scanner.nextLine());
+           System.out.print("Enter a street name: "); streetName = validateInput(scanner.nextLine());
 
            System.out.print("Do you wish to enter an apartment number? (y/n): ");
            if (scanner.nextLine().equalsIgnoreCase("y")) {
                System.out.print("Enter an apartment number: ");
-               apartmentNumber = scanner.nextLine();
+               apartmentNumber = validateInput(scanner.nextLine());
            } else apartmentNumber = "";
 
-           System.out.println("Enter a city: "); city = scanner.nextLine();
-           System.out.println("Enter a state: "); state = scanner.nextLine();
-           System.out.println("Enter a zipcode: "); zipcode = scanner.nextLine();
+           System.out.println("Enter a city: "); city = validateInput(scanner.nextLine());
+           System.out.println("Enter a state: "); state = validateInput(scanner.nextLine());
+           System.out.println("Enter a zipcode: "); zipcode = validateInput(scanner.nextLine());
 
+           String addressString = streetNumber + ", " + streetName + ", ";
+           if (!apartmentNumber.equals("")) addressString += apartmentNumber + ", ";
+           addressString += city + ", " + state + ", " + zipcode;
 
+           address = new Address(addressString);
+
+           System.out.println(address);
     }
 
-    public static String validateAddressInput(String input) {
+    public static String validateInput(String input) {
         String inputArray[] = input.split(" ");
         if (inputArray.length > 1) return inputArray[0] + "-" + inputArray[1];
         return input;
